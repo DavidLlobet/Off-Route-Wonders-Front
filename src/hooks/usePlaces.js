84cfaf68779a2b81c2824efe5e017e8866cd3957
David <david.llobet6@gmail.com>
2021-11-28 17:26:18 +0100

    Create usePlaces hook to dispatch loadPlacesThunk

---
+++ src/hooks/usePlaces.js
@@ -0,0 +1,16 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadPlacesThunk } from "../redux/thunks/placesThunks";
+
+const usePlaces = () => {
+  const places = useSelector((store) => store.place);
+  const dispatch = useDispatch();
+
+  const loadPlaces = useCallback(() => {
+    dispatch(loadPlacesThunk);
+  }, [dispatch]);
+
+  return { places, loadPlaces };
+};
+
+export default usePlaces;
