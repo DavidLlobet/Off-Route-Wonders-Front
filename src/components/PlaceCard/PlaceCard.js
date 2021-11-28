a08bd76003d0a489d0eb34b518ce683d5476430c
David <david.llobet6@gmail.com>
2021-11-28 18:13:40 +0100

    Create basic structure for PlaceCard and PlacesList components

---
+++ src/components/PlaceCard/PlaceCard.js
@@ -0,0 +1,11 @@
+const PlaceCard = () => {
+  return (
+    <li className="card">
+      <p className="card__title"></p>
+      <img src="" alt="" className="card__photo" />
+      <p className="card__info"></p>
+    </li>
+  );
+};
+
+export default PlaceCard;
---
+++ src/components/PlacesList/PlacesList.js
@@ -0,0 +1,10 @@
+const PlacesList = () => {
+  return (
+    <>
+      <h1>Entradas recientes</h1>
+      <ul className="list"></ul>
+    </>
+  );
+};
+
+export default PlacesList;
