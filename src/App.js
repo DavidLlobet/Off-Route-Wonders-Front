029805ed398c2b11d5e6cf7a520c3bef2d7c4cc5
David <david.llobet6@gmail.com>
2021-11-29 15:19:08 +0100

    Create Header component and add it to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/home" element={<HomePage />} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p className="header__logo">Off Route Wonders</p>
+    </header>
+  );
+};
+
+export default Header;
