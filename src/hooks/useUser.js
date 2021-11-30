ffa5866ebde19a90526b83cc36f0d6597e39a8fa
David <david.llobet6@gmail.com>
2021-11-30 17:13:10 +0100

    Create useUser hook

---
+++ src/hooks/useUser.js
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { loginUserThunk } from "../redux/thunks/userThunks";
+
+const useUser = () => {
+  const user = useSelector(({ user }) => user);
+  const dispatch = useDispatch();
+
+  const loginUser = (user) => {
+    dispatch(loginUserThunk(user));
+  };
+  return {
+    user,
+    loginUser,
+  };
+};
+
+export default useUser;
