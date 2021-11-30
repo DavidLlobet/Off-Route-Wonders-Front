bfa26a4816d010d134f21753566dd1b3dbd05604
David <david.llobet6@gmail.com>
2021-11-30 12:20:43 +0100

    Create action Type and creator for loadPlacesByCountry plus test

--- src/redux/actions/actionCreators.test.js
+++ src/redux/actions/actionCreators.test.js
@@ -1,4 +1,4 @@
-import { loadPlacesAction } from "./actionCreators";
+import { loadPlacesAction, loadPlacesByCountryAction } from "./actionCreators";
 import actionTypes from "./actionTypes";
 
 describe("Given an actionCreators", () => {
@@ -77,5 +77,83 @@ describe("Given an actionCreators", () => {
 
       expect(result).toEqual(expected);
     });
+  });
+});
+
+describe("When it calls a loadPlacesByCountryAction", () => {
+  test("Then it should return a list of places", () => {
+    const idCountry = "61a1057eec1d9f99c7672ca6";
+    // const initialPlaces = [
+    //   {
+    //     coordinates: {
+    //       longitude: 123443454,
+    //       latitude: 123454354,
+    //     },
+    //     author: {
+    //       username: "Mario González",
+    //     },
+    //     title: "Blue Eye Theth",
+    //     country: {
+    //       name: "Albania",
+    //     },
+    //     images: [
+    //       "https://storage.googleapis.com/off-route-wonders.appspot.com/dylan-gillis-3e_tXW5O3GQ-unsplash-1638035068556-.jpg",
+    //       "https://storage.googleapis.com/off-route-wonders.appspot.com/2021105724573_1 (1)-1638035068569-.jpg",
+    //     ],
+    //     text: "Mola mucho",
+    //     comments: [
+    //       {
+    //         text: "¡Que guapo!",
+    //         author: {
+    //           username: "Marc",
+    //           id: "619fd3f198bdb970bc0c248e",
+    //         },
+    //         date: "2021-11-27T19:08:31.344Z",
+    //         id: "61a271aeec1d9f99c7672cea",
+    //       },
+    //     ],
+    //     date: "2021-11-27T17:44:29.550Z",
+    //     id: "61a26e7d1c9f711f24d1a8fc",
+    //   },
+    //   {
+    //     coordinates: {
+    //       longitude: 123443454,
+    //       latitude: 123454354,
+    //     },
+    //     author: {
+    //       username: "Mario González",
+    //     },
+    //     title: "Paradise Cave",
+    //     country: {
+    //       name: "Albania",
+    //     },
+    //     images: [
+    //       "https://storage.googleapis.com/off-route-wonders.appspot.com/dylan-gillis-3e_tXW5O3GQ-unsplash-1638035068556-.jpg",
+    //       "https://storage.googleapis.com/off-route-wonders.appspot.com/2021105724573_1 (1)-1638035068569-.jpg",
+    //     ],
+    //     text: "Mola mucho",
+    //     comments: [
+    //       {
+    //         text: "¡Que guapo!",
+    //         author: {
+    //           username: "Marc",
+    //           id: "619fd3f198bdb970bc0c248e",
+    //         },
+    //         date: "2021-11-27T19:08:31.344Z",
+    //         id: "61a271aeec1d9f99c7672cea",
+    //       },
+    //     ],
+    //     date: "2021-11-27T17:44:29.550Z",
+    //     id: "61a26e7d1c9f711f24d1a8fc",
+    //   },
+    // ];
+    const expected = {
+      type: actionTypes.loadPlacesByCountry,
+      id: idCountry,
+    };
+
+    const result = loadPlacesByCountryAction(idCountry);
+
+    expect(result).toEqual(expected);
   });
 });
--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,5 +1,6 @@
 const actionTypes = {
   loadPlaces: "LOAD_PLACES",
+  loadPlacesByCountry: "LOAD_PLACES_BY_COUNTRY",
 };
 
 export default actionTypes;
