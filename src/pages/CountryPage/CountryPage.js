b8bf9a2d2d3c0cff0eb6fa8b1a73ac7530e2439a
David <david.llobet6@gmail.com>
2021-11-30 11:45:12 +0100

    Create basic structure for Country Page

---
+++ src/pages/CountryPage/CountryPage.js
@@ -0,0 +1,11 @@
+import FilterButton from "../../components/FilterButton/FilterButton";
+
+const CountryPage = () => {
+  return (
+    <div>
+      <FilterButton />
+    </div>
+  );
+};
+
+export default CountryPage;
