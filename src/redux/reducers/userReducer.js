b16ec89d8e45c9af40bde90dac1a3550c819e0a0
David <david.llobet6@gmail.com>
2021-11-30 17:04:50 +0100

    Create loginUserThunk and userReducer

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,17 @@
+import actionTypes from "../actions/actionTypes";
+
+const userReducer = (user = { isAuthenticated: false, user: {} }, action) => {
+  let newUser;
+
+  switch (action.type) {
+    case actionTypes.loginUser:
+      newUser = { user: action.user, isAuthenticated: true };
+      break;
+    default:
+      newUser = { ...user };
+      break;
+  }
+
+  return newUser;
+};
+export default userReducer;
