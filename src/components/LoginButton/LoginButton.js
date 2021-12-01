ea2fe255b4351c7ea4c9cac1f7eacf86e2f27ce6
David <david.llobet6@gmail.com>
2021-12-01 17:30:12 +0100

    Add conditional rendering in LoginButton to check if logged or not

--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./LoginButton.scss";
 
 const LoginButton = () => {
-  return (
+  const { isAuthenticated } = useSelector((store) => store.user);
+  return !isAuthenticated ? (
     <Link className="login" to="login">
       Entra
+    </Link>
+  ) : (
+    <Link className="login" to="my-profile">
+      My perfil
     </Link>
   );
 };
