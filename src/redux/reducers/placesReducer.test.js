ed555b7774d06ad2d06ca613295bcb9afeb77d46
David <david.llobet6@gmail.com>
2021-12-02 21:01:47 +0100

    Add createPlace action to placesReducer

--- src/redux/reducers/placesReducer.test.js
+++ src/redux/reducers/placesReducer.test.js
@@ -1,4 +1,5 @@
 import {
+  createPlaceAction,
   loadPlacesAction,
   loadPlacesByCountryAction,
 } from "../actions/actionCreators";
@@ -149,6 +150,110 @@ describe("Given a placesReducer reducer", () => {
       const newPlaces = placesReducer([], loadPlacesByCountryAction(places));
 
       expect(newPlaces).toEqual(places);
+    });
+  });
+  describe("When it receives a list of places and a createPlaceAction", () => {
+    test("Then it should return a new list with the new place included", () => {
+      const places = [
+        {
+          coordinates: {
+            longitude: 123443454,
+            latitude: 123454354,
+          },
+          author: {
+            username: "Mario González",
+          },
+          title: "Blue Eye Theth",
+          country: {
+            name: "Albania",
+          },
+          images: [
+            "https://storage.googleapis.com/off-route-wonders.appspot.com/dylan-gillis-3e_tXW5O3GQ-unsplash-1638035068556-.jpg",
+            "https://storage.googleapis.com/off-route-wonders.appspot.com/2021105724573_1 (1)-1638035068569-.jpg",
+          ],
+          text: "Mola mucho",
+          comments: [
+            {
+              text: "¡Que guapo!",
+              author: {
+                username: "Marc",
+                id: "619fd3f198bdb970bc0c248e",
+              },
+              date: "2021-11-27T19:08:31.344Z",
+              id: "61a271aeec1d9f99c7672cea",
+            },
+          ],
+          date: "2021-11-27T17:44:29.550Z",
+          id: "61a26e7d1c9f711f24d1a8fc",
+        },
+        {
+          coordinates: {
+            longitude: 123443454,
+            latitude: 123454354,
+          },
+          author: {
+            username: "Mario González",
+          },
+          title: "Paradise Cave",
+          country: {
+            name: "Vietnam",
+          },
+          images: [
+            "https://storage.googleapis.com/off-route-wonders.appspot.com/dylan-gillis-3e_tXW5O3GQ-unsplash-1638035068556-.jpg",
+            "https://storage.googleapis.com/off-route-wonders.appspot.com/2021105724573_1 (1)-1638035068569-.jpg",
+          ],
+          text: "Mola mucho",
+          comments: [
+            {
+              text: "¡Que guapo!",
+              author: {
+                username: "Marc",
+                id: "619fd3f198bdb970bc0c248e",
+              },
+              date: "2021-11-27T19:08:31.344Z",
+              id: "61a271aeec1d9f99c7672cea",
+            },
+          ],
+          date: "2021-11-27T17:44:29.550Z",
+          id: "61a26e7d1c9f711f24d1a8fc",
+        },
+      ];
+      const place = {
+        coordinates: {
+          longitude: 123443454,
+          latitude: 123454354,
+        },
+        author: {
+          username: "Mario González",
+        },
+        title: "Green Eye Theth",
+        country: {
+          id: "61a1057eec1d9f99c7672ca6",
+          name: "Albania",
+        },
+        images: [
+          "https://storage.googleapis.com/off-route-wonders.appspot.com/dylan-gillis-3e_tXW5O3GQ-unsplash-1638035068556-.jpg",
+          "https://storage.googleapis.com/off-route-wonders.appspot.com/2021105724573_1 (1)-1638035068569-.jpg",
+        ],
+        text: "Mola mucho",
+        comments: [
+          {
+            text: "¡Que guapo!",
+            author: {
+              username: "Marc",
+              id: "619fd3f198bdb970bc0c248e",
+            },
+            date: "2021-11-27T19:08:31.344Z",
+            id: "61a271aeec1d9f99c7672cea",
+          },
+        ],
+        date: "2021-11-27T17:44:29.550Z",
+        id: "61a26e7d1c9f711f24d1a8fc",
+      };
+
+      const newPlaces = placesReducer(places, createPlaceAction(place));
+
+      expect(newPlaces).toContain(place);
     });
   });
 });
