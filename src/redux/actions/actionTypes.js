60c96e71e810f1fe585da8ee5d4e97d068402d03
David <david.llobet6@gmail.com>
2021-12-04 12:12:19 +0100

    Create action type and creator for loadPlacesByAuthor plus test

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,6 +1,7 @@
 const actionTypes = {
   loadPlaces: "LOAD_PLACES",
   loadPlacesByCountry: "LOAD_PLACES_BY_COUNTRY",
+  loadPlacesByAuthor: "LOAD_PLACES_BY_AUTHOR",
   createPlace: "CREATE_PLACE",
   loginUser: "LOGIN_USER",
   registerUser: "REGISTER_USER",
