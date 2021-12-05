4bd6ee1efe94276880f19ab2fe2ff4aeafa11f0f
David <david.llobet6@gmail.com>
2021-12-05 18:57:06 +0100

    Create action type and creator plus test for updatePlace

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -4,6 +4,7 @@ const actionTypes = {
   loadPlacesByAuthor: "LOAD_PLACES_BY_AUTHOR",
   loadPlace: "LOAD_PLACE",
   createPlace: "CREATE_PLACE",
+  updatePlace: "UPDATE_PLACE",
   deletePlace: "DELETE_PLACE",
   loginUser: "LOGIN_USER",
   registerUser: "REGISTER_USER",
