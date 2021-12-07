1d4083e20cb98f44fbfc93bcc0aa20a96eeb3e3d
David <david.llobet6@gmail.com>
2021-12-07 22:43:33 +0100

    Apply CSS to forms components

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -38,42 +38,44 @@ const LoginForm = () => {
 
   return (
     <>
-      <p className="title">
-        Logéate para compartir tus experiencias por el mundo
-      </p>
-      <form
-        className="login-form"
-        noValidate
-        autoComplete="off"
-        onSubmit={onSubmit}
-      >
-        <label htmlFor="username"></label>
-        <input
-          type="text"
-          id="username-login"
-          placeholder="Introduce tu nombre de usuario"
-          name="username"
-          className="login-form__input"
-          value={userData.username}
-          required
-          onChange={changeUserData}
-        ></input>
-        <label htmlFor="password"></label>
-        <input
-          type="password"
-          id="password-login"
-          placeholder="Introduce tu contraseña"
-          name="password"
-          className="login-form__input"
-          value={userData.password}
-          required
-          onChange={changeUserData}
-        ></input>
-        <button type="submit" className="login-form__button">
-          Validar
-        </button>
-      </form>
-      <div className="line"></div>
+      <div className="login-container">
+        <p className="login-title">
+          Logéate para compartir tus experiencias por el mundo
+        </p>
+        <form
+          className="login-form"
+          noValidate
+          autoComplete="off"
+          onSubmit={onSubmit}
+        >
+          <label htmlFor="username"></label>
+          <input
+            type="text"
+            id="username-login"
+            placeholder="Introduce tu nombre de usuario"
+            name="username"
+            className="login-form__input"
+            value={userData.username}
+            required
+            onChange={changeUserData}
+          ></input>
+          <label htmlFor="password"></label>
+          <input
+            type="password"
+            id="password-login"
+            placeholder="Introduce tu contraseña"
+            name="password"
+            className="login-form__input"
+            value={userData.password}
+            required
+            onChange={changeUserData}
+          ></input>
+          <button type="submit" className="login-form__button">
+            Validar
+          </button>
+        </form>
+        <div className="line"></div>
+      </div>
     </>
   );
 };
